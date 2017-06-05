d0daf0b20ac35cad97faa3c2bda14cccfcddf58e
Sebastian <krewllobster@gmail.com>
2017-06-05 16:46:30 -0400

    add sort and reverse sort and fix tests on table

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
       searchTerm: DEFAULT_QUERY,
       isLoading: false,
       sortKey: 'NONE',
+      isSortReverse: false,
     };
 
     this.needsToSearchTopstories = this.needsToSearchTopstories.bind(this);
@@ -45,7 +46,8 @@ class App extends Component {
   }
 
   onSort(sortKey) {
-    this.setState({ sortKey });
+    const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse;
+    this.setState({ sortKey, isSortReverse });
   }
 
   setSearchTopstories(result) {
@@ -115,7 +117,14 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, results, searchKey, isLoading, sortKey } = this.state;
+    const {
+      searchTerm,
+      results,
+      searchKey,
+      isLoading,
+      sortKey,
+      isSortReverse
+    } = this.state;
 
     const page = (
       results &&
@@ -141,6 +150,7 @@ class App extends Component {
             <Table
               hits={list}
               sortKey={sortKey}
+              isSortReverse={isSortReverse}
               onSort={this.onSort}
               onDismiss={this.onDismiss}
             />
@@ -213,49 +223,61 @@ const Sort = ({ sortKey, onSort, children}) => (
   </Button>
 )
 
-const Table = ({hits, onDismiss, sortKey, onSort}) => {
+const Table = ({
+  hits,
+  onDismiss,
+  sortKey,
+  isSortReverse,
+  onSort
+}) => {
   const largeColumn = {width: '40%'};
   const midColumn = {width: '30%'};
   const smallColumn = {width: '10%'};
+  const sortedHits = SORTS[sortKey](hits);
+  const reverseSortedHits = isSortReverse
+    ? sortedHits.reverse()
+    : sortedHits;
+
   return (
-  <div className="table">
-    <div className = "table-header">
-      <span style={largeColumn}>
-        <Sort sortKey={'TITLE'} onSort={onSort}>Title</Sort>
-      </span>
-      <span style={midColumn}>
-        <Sort sortKey={'AUTHOR'} onSort={onSort}>Author</Sort>
-      </span>
-      <span style={smallColumn}>
-        <Sort sortKey={'COMMENTS'} onSort={onSort}>Comments</Sort>
-      </span>
-      <span style={smallColumn}>
-        <Sort sortKey={'POINTS'} onSort={onSort}>Points</Sort>
-      </span>
-      <span style={smallColumn}>
-        Archive
-      </span>
-    </div>
-    {hits.map(item =>
-      <div key={item.objectID} className="table-row">
+    <div className="table">
+      <div className = "table-header">
         <span style={largeColumn}>
-          <a href={item.url} target="_blank">{item.title}</a>
+          <Sort sortKey={'TITLE'} onSort={onSort}>Title</Sort>
         </span>
-        <span style={midColumn}>{item.author}</span>
-        <span style={smallColumn}>{item.num_comments}</span>
-        <span style={smallColumn}>{item.points}</span>
+        <span style={midColumn}>
+          <Sort sortKey={'AUTHOR'} onSort={onSort}>Author</Sort>
+        </span>
         <span style={smallColumn}>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-            className="button-inline"
-          >
-            Dismiss
-          </Button>
+          <Sort sortKey={'COMMENTS'} onSort={onSort}>Comments</Sort>
+        </span>
+        <span style={smallColumn}>
+          <Sort sortKey={'POINTS'} onSort={onSort}>Points</Sort>
+        </span>
+        <span style={smallColumn}>
+          Archive
         </span>
       </div>
-  )}
-  </div>
-)}
+      {reverseSortedHits.map(item =>
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
+            <a href={item.url} target="_blank">{item.title}</a>
+          </span>
+          <span style={midColumn}>{item.author}</span>
+          <span style={smallColumn}>{item.num_comments}</span>
+          <span style={smallColumn}>{item.points}</span>
+          <span style={smallColumn}>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
+              Dismiss
+            </Button>
+          </span>
+        </div>
+    )}
+    </div>
+  );
+}
 
 Table.propTypes = {
   hits: PropTypes.arrayOf(
